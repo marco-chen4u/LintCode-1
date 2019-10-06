2fe162be4352a8cf24721b5d9b184276eb05cfcc
GaryGuo007 <guoxin89749@gmail.com>
2019-10-05 19:16:52 -0500

    8. Rotate String

---
+++ A008_RotateString.java
@@ -0,0 +1,28 @@
+public class rotateString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = new String("abcdefg");
+		char[] str =s.toCharArray();
+		rotateString(str,3);
+
+	}
+	
+	public static void rotateString(char[] str, int offset) {
+        // write your code here
+        char temp;
+        if(str.length == 0) return;
+        if(offset == 0) return;
+        if(str.length < offset) offset = offset % str.length;
+        while(offset>0){
+            temp = str[str.length-1];
+            for(int i = str.length - 1; i > 0;i--){
+                str[i] = str[i-1];
+            }
+            str[0]=temp;
+            offset--;
+            System.out.println(str);
+        }
+    }
+
+}
