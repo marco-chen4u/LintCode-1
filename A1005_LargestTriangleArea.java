2d8343b16a1b9f49493fffdc0b802b48c91b4bc5
Xin Guo <guoxin89749@gmail.com>
2019-10-11 11:55:41 -0500

    Update A1005_LargestTriangleArea.java

--- A1005_LargestTriangleArea.java
+++ A1005_LargestTriangleArea.java
@@ -19,6 +19,8 @@ public class A1005_LargestTriangleArea {
 	       for (int[] x : points)
 	           for (int[] y : points)
 	               for (int[] z : points)
+			       //https://zhidao.baidu.com/question/93368735.html
+			       // 三角形面积公式 S = 1/2 ((x1y2 - x2y1) + (y1z2 - y2z1) + (z1x2 - z2x1))
 	                   res = Math.max(res, 0.5 * (x[0] * y[1] + y[0] * z[1] + z[0] * x[1]
 	                           - x[0] * z[1] - y[0] * x[1] - z[0] * y[1]));
 	       return res;
