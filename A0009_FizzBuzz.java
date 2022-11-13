a6daeb0d1b8ea35f21d252c75f31836b86489deb
Marco Chen <haisheng.chen4u@foxmail.com>
2022-11-13 13:40:24 -0600

    Update A0009_FizzBuzz.java

--- A0009_FizzBuzz.java
+++ A0009_FizzBuzz.java
@@ -3,6 +3,34 @@ package lintCodeWithMrChen;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/***
+*LintCode-9 · Fizz Buzz
+*Given number n. Print number from 1 to n. According to following rules:
+
+*when number is divided by 3, print "fizz".
+*when number is divided by 5, print "buzz".
+*when number is divided by both 3 and 5, print "fizz buzz".
+*when number can't be divided by either 3 or 5, print the number itself.
+	
+*Example 1:
+*	Input:
+*		n = 15
+*	Output:
+*		[
+*		  "1", "2", "fizz",
+*		  "4", "buzz", "fizz",
+*		  "7", "8", "fizz",
+*		  "buzz", "11", "fizz",
+*		  "13", "14", "fizz buzz"
+*		]
+*
+*Challenge
+*	Can you do it with only one if statement?
+***/	
+
+			
+//solution-1: for loop with if-esle 			
 public class A0009_FizzBuzz {
 	public List<String> fizzBuzz(int n){
 		List<String> list = new ArrayList<String>();
